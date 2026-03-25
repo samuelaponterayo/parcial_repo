ef48973e67c074ed2da8bd6bc4d5bddfe84534e3
Samuel <samuel.aponte@utp.edu.co>
2026-03-24 19:34:24 -0500

    Arreglando identación, gestion de dependencias y .gitignore

--- main.py
+++ main.py
@@ -4,10 +4,10 @@ app = FastAPI()
 load_dotenv() 
 @app.get("/llm/{prompt}") 
 async def read_root(prompt): 
-from google import genai 
+        from google import genai 
 # The client gets the API key from the environment variable `GEMINI_API_KEY`. client = genai.Client() 
-response = client.models.generate_content( 
-model="gemini-3-flash-preview", contents=prompt 
-) 
-print(response.text) 
-return {"Respuesta": response.text} 
+        response = client.models.generate_content( 
+        model="gemini-3-flash-preview", contents=prompt 
+        ) 
+        print(response.text) 
+        return {"Respuesta": response.text} 
